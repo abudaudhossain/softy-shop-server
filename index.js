205b86ddd7dc5ee7993d586a2d62142434f2ebdf
abudaudhossain <abudaudhossainsumon@gmail.com>
2022-01-01 12:29:43 +0600

    connet to database

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bqqvk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("connection success")
+
+        
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
     res.send("Softy shop");
