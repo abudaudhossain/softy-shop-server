dcc16b1184cbc454d3f74aca2c2e34549c240e46
abudaudhossain <abudaudhossainsumon@gmail.com>
2022-01-04 19:01:10 +0600

    add some api

--- index.js
+++ index.js
@@ -25,6 +25,20 @@ async function run() {
         const orderCollection = database.collection('orders');
         const reviewCollection = database.collection('review');
 
+        //post addProduct api
+        app.post('/addProduct', async (req, res) => {
+            const product = req.body
+            const result = await productsCollection.insertOne(product);
+            res.json(result);
+        })
+        //delete my Product api
+        app.delete('/deleteProduct/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+
+            res.json(result)
+        })
         // get all products api 
         app.get('/products', async (req, res) => {
             const cursor = productsCollection.find({});
@@ -42,9 +56,9 @@ async function run() {
         })
 
         //get product by category
-        app.get('/products/category/:category', async(req,res) =>{
+        app.get('/products/category/:category', async (req, res) => {
             const category = req.params.category;
-            const query = {category: category}
+            const query = { category: category }
             const result = await productsCollection.find(query).toArray();
 
             res.send(result);
@@ -59,9 +73,9 @@ async function run() {
         })
         // get user by email api 
         app.get('/user/:email', async (req, res) => {
-            
+
             const email = req.params.email;
-            const query = { email: email}
+            const query = { email: email }
             const result = await usersCollection.find(query).toArray();
 
             res.send(result);
@@ -75,17 +89,20 @@ async function run() {
 
 
         //Order api 
-          app.post('/addOrder', async (req, res) => {
+        app.post('/addOrder', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result)
         })
-        //get order list for user api
-        app.post('/myOrder', async (req, res) => {
-            const email = req.body;
-            // const query = {};
-            const result = await orderCollection.find(email).toArray();
-            res.send(result)
+
+        // get user by email api 
+        app.get('/myOrder/:email', async (req, res) => {
+
+            const email = req.params.email;
+            const query = { email: email }
+            const result = await orderCollection.find(query).toArray();
+
+            res.send(result);
         })
         //get all order
         app.get('/allOrders', async (req, res) => {
@@ -94,6 +111,45 @@ async function run() {
             res.send(result)
         })
 
+        //delete my order api
+        app.delete('/deleteOrder/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+
+            res.json(result)
+        })
+
+        //update states api
+        app.put('/updateStates/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    orderStates: `Shipped`
+                },
+            };
+            const result = await orderCollection.updateOne(query, updateDoc, options);
+            console.log("heading shipped");
+
+            res.json(result);
+        })
+        //update admin states api
+        app.put('/updateAdmin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            console.log(query);
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    roll: "admin"
+                },
+            };
+            const result = await usersCollection.updateOne(query, updateDoc, options);
+
+            res.json(result);
+        })
 
     } finally {
         // await client.close();
